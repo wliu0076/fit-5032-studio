4772522b97f7070ba91c20209f891c37a989c404
wliu0076 <wliu0076@student.monash.edu>
2024-08-01 18:34:25 +1000

    wk1

---
+++ my-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="welcome to my vue project, week1" />
+    <MyName /> 
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MyName from './components/MyName.vue' 
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MyName 
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-project/src/components/MyName.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Hello, my name is Wei Liu
+    </h1>
+    <h1>student#33555508
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
